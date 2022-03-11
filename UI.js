5a4bce8dfa24911c1dca5a4aef2c3839557552c0
ZukangZ <zukangzhou2@gmail.com>
2022-03-11 18:05:36 -0500

    sticky navbar, move listeners to js

--- UI.js
+++ UI.js
@@ -1,9 +1,33 @@
-function openProj(projName) {
-    let i;
-    var x = document.getElementById('projects-content').children;
+//click listeners for proj buttons
+const projButts = document.getElementById("projects-buttons").getElementsByTagName("button");
+const projs = document.getElementById("projects-content").getElementsByTagName("div");
+//discard declared vars after code block
+// {
+//     const butts = document.getElementById("projects-buttons").getElementsByTagName("button");
+//     const len = butts.length > projs.length ? butts.length : projs.length;
 
-    for (i = 0; i < x.length; i++) 
-        x[i].style.display = "none";
-    
-    document.getElementById(projName).style.display = "inline";
+//     for(let i = 0; i < len; i++)
+//         projButts[i] = [butts[i], projs[i]];        
+// }
+let activeProj = projs[0];
+
+for(let i = 0; i < projButts.length; i++) {
+    projButts[i].addEventListener("click", (e) => {
+        const currProj = e.target;
+
+        //do nothing if clicking the same butt
+        if(activeProj.id === currProj.id) {
+            console.log("deez nuts");
+            return;
+        }
+        for(let i = 0; i < projs.length; i++)
+            if(projs[i].id === currProj.id) {
+                activeProj.style.display = "none";
+                projs[i].style.display = "inline";
+                activeProj = projs[i];
+                break;
+            }
+        
+        console.log(this, e);
+    });
 }
